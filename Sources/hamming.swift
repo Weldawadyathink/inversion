bcae2be74553d92858f6ba9b694f635341c899a4
Spenser Bushey <spenser@spenserbushey.com>
2025-06-28 16:28:53 +0200

    Separate hamming functions to another file

---
+++ Sources/hamming.swift
@@ -0,0 +1,125 @@
+import Foundation
+
+extension Data {
+    func bit(at bitIndex: Int) -> UInt8 {
+        let byteIndex = bitIndex / 8
+        let bitInByte = 7 - (bitIndex % 8)
+        return (self[byteIndex] >> bitInByte) & 1
+    }
+    mutating func setBit(at bitIndex: Int, to value: UInt8) {
+        let byteIndex = bitIndex / 8
+        let bitInByte = 7 - (bitIndex % 8)
+        if value == 1 {
+            self[byteIndex] |= (1 << bitInByte)
+        } else {
+            self[byteIndex] &= ~(1 << bitInByte)
+        }
+    }
+    var binaryString: String {
+        self.map { String($0, radix: 2).leftPad(toLength: self.count * 8, withPad: "0") }.joined()
+    }
+}
+
+extension String {
+    func leftPad(toLength: Int, withPad character: Character) -> String {
+        if self.count < toLength {
+            return String(repeatElement(character, count: toLength - self.count)) + self
+        } else {
+            return self
+        }
+    }
+}
+
+public struct Hamming {
+    public static func buildParityBlock(data: Data, parity: Data) throws -> Data {
+        guard data.count == 15 else {
+            throw NSError(
+                domain: "HammingParityError", code: 1,
+                userInfo: [NSLocalizedDescriptionKey: "Data must be 120 bits"])
+        }
+        guard parity.count == 1 else {
+            throw NSError(
+                domain: "HammingParityError", code: 1,
+                userInfo: [NSLocalizedDescriptionKey: "Parity must be 8 bits"])
+        }
+        var output = Data(count: 16)
+        var nextDataBitToRead = 0
+        for i in 0..<128 {
+            if i == 0 {
+                output.setBit(at: i, to: parity.bit(at: 0))
+            } else if (i & (i - 1)) == 0 {
+                output.setBit(at: i, to: parity.bit(at: i.trailingZeroBitCount + 1))
+            } else {
+                output.setBit(at: i, to: data.bit(at: nextDataBitToRead))
+                nextDataBitToRead += 1
+            }
+        }
+        return output
+    }
+
+    public static func generateParityBits(_ input: Data) throws -> Data {
+        guard input.count == 15 else {
+            throw NSError(
+                domain: "HammingParityError", code: 1,
+                userInfo: [NSLocalizedDescriptionKey: "Input must be 120 bits"])
+        }
+        var bits = try buildParityBlock(data: input, parity: Data(count: 1))
+        for p in 0..<7 {
+            let parityPosition = 1 << p
+            var parityValue: UInt8 = 0
+            for i in 1..<128 {
+                if (i & parityPosition) != 0 {
+                    parityValue ^= bits.bit(at: i)
+                }
+            }
+            bits.setBit(at: parityPosition, to: parityValue)
+        }
+        return bits
+    }
+
+    public static func calculateFileParity(from filename: String) throws -> [(
+        parityBits: Data, dataBits: Data
+    )] {
+        let url = URL(fileURLWithPath: filename)
+        let fileData = try Data(contentsOf: url)
+        let chunkSize = 15
+        var blocks: [(parityBits: Data, dataBits: Data)] = []
+        var offset = 0
+        while offset < fileData.count {
+            let end = min(offset + chunkSize, fileData.count)
+            var chunk = fileData.subdata(in: offset..<end)
+            if chunk.count < chunkSize {
+                chunk.append(contentsOf: [UInt8](repeating: 0, count: chunkSize - chunk.count))
+            }
+            let parity = try generateParityBits(chunk)
+            let (parityBits, dataBits) = try extractParityBits(parity)
+            blocks.append((parityBits, dataBits))
+            offset += chunkSize
+        }
+        return blocks
+    }
+
+    public static func extractParityBits(_ input: Data) throws -> (parityBits: Data, dataBits: Data)
+    {
+        guard input.count == 16 else {
+            throw NSError(
+                domain: "HammingParityError", code: 1,
+                userInfo: [NSLocalizedDescriptionKey: "Block must be 16 bits"])
+        }
+        var parityBits: Data = Data(count: 1)
+        var dataBits: Data = Data(count: 15)
+        var dataBitToSetNext = 0
+        for i in 0..<128 {
+            if i == 0 {
+                parityBits.setBit(at: 0, to: input.bit(at: 0))
+            } else if (i & (i - 1)) == 0 {
+                parityBits.setBit(
+                    at: i.trailingZeroBitCount + 1, to: input.bit(at: i.trailingZeroBitCount + 1))
+            } else {
+                dataBits.setBit(at: dataBitToSetNext, to: input.bit(at: i))
+                dataBitToSetNext += 1
+            }
+        }
+        return (parityBits, dataBits)
+    }
+}
