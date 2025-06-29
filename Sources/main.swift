a0e00c80f683f7c4e780c519b3d8aababf6a1384
Spenser Bushey <spenser@spenserbushey.com>
2025-06-29 08:10:46 +0200

    Separate HammingFileProvider

--- Sources/main.swift
+++ Sources/main.swift
@@ -6,7 +6,7 @@ import GRDB
 
 let db = Database()
 
-let hams = try HammingFile(filename: "test-initial.txt")
+let hams = try HammingFileProvider(filename: "test-initial.txt")
 print("Initial:")
 hams.prettyPrint()
 
@@ -14,11 +14,11 @@ try db.saveParity(parities: hams.parities)
 
 let parities = try db.getParities()
 
-let hams2 = try HammingFile(filename: "test-initial.txt", parities: parities)
+let hams2 = try HammingFileProvider(filename: "test-initial.txt", parities: parities)
 print("Initial (reloaded):")
 hams2.prettyPrint()
 
-let hams3 = try HammingFile(filename: "test-bitrot.txt", parities: parities)
+let hams3 = try HammingFileProvider(filename: "test-bitrot.txt", parities: parities)
 print("Bitrot:")
 hams3.prettyPrint()
 let results = hams3.checkParity(repair: true)
